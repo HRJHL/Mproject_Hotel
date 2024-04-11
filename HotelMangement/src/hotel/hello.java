defe4456d29f5d7c78da6dfe780c741a17de1a2e
한영욱 <70461240+10wook@users.noreply.github.com>
2024-04-11 16:26:30 +0900

    Update hello.java

--- HotelMangement/src/hotel/hello.java
+++ HotelMangement/src/hotel/hello.java
@@ -5,22 +5,21 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("잘보이시나요");
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-		System.out.println("네");
-=======
 
 		System.out.println("네");
 
->>>>>>> Stashed changes
+
+		System.out.println("네");
+
+
 		System.out.println("한영욱 업로드");
-=======
+
 
 		System.out.println("한영욱 업로드");
 		System.out.println("장호영 업로드");
 		System.out.println("네");
 
->>>>>>> cc2275f78a86d1eb7bdb8466c97ffcb34bbeeac5
+
 	}
 
 }
