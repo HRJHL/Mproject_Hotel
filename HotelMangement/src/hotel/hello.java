b304b16ca4f4f6dbf0f115d307687494ff72e1d6
wonhong94 <wonhong1994@naver.com>
2024-04-11 15:37:34 +0900

    Update hello.java

--- HotelMangement/src/hotel/hello.java
+++ HotelMangement/src/hotel/hello.java
@@ -5,7 +5,13 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("잘보이시나요");
+<<<<<<< Updated upstream
 		System.out.println("네");
+=======
+
+		System.out.println("네");
+
+>>>>>>> Stashed changes
 		System.out.println("한영욱 업로드");
 	}
 
