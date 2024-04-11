6e9202343b55d1fcf8fdafb2a91f009bd33c0ae3
장호영 <jang643@naver.com>
2024-04-11 15:51:38 +0900

    .
    .

--- HotelMangement/src/hotel/hello.java
+++ HotelMangement/src/hotel/hello.java
@@ -4,7 +4,11 @@ public class hello {
 
 	public static void main(String[] args) {
 		System.out.println("hello");
-		System.out.println("안녕하세요");
+		System.out.println("잘보이시나요");
+		System.out.println("네");
+		System.out.println("한영욱 업로드");
+		System.out.println("장호영 업로드");
+		System.out.println("장호영 업로드2");
 	}
 
 }
