32c5a64e22483b6df456d1bc92299bcc2a5001e2
장호영 <jang643@naver.com>
2024-04-15 11:33:41 +0900

    .

--- hotel/src/com/dto/GuestDTO.java
+++ hotel/src/com/dto/GuestDTO.java
@@ -2,78 +2,77 @@ package com.dto;
 
 public class GuestDTO {
 
-	
 	String guest_pno;
 	int m_count;
 	String guest_name;
 	int stay_days;
-	String parking_yn; 
+	String car_no;
 	int room_no;
-	
-	public GuestDTO() {}  // 기본  생성자 필수 (**************)
 
-	    public GuestDTO(String guest_pno, int m_count, String guest_name, int stay_days, String parking_yn, int room_no) {
-	        this.guest_pno = guest_pno;
-	        this.m_count = m_count;
-	        this.guest_name = guest_name;
-	        this.stay_days = stay_days;
-	        this.parking_yn = parking_yn;
-	        this.room_no = room_no;
-	    }
+	public GuestDTO() {
+	} // 기본 생성자 필수 (**************)
 
-	    public String getGuest_pno() {
-	        return guest_pno;
-	    }
+	public GuestDTO(String guest_pno, int m_count, String guest_name, int stay_days, String car_no, int room_no) {
+		this.guest_pno = guest_pno;
+		this.m_count = m_count;
+		this.guest_name = guest_name;
+		this.stay_days = stay_days;
+		this.car_no = car_no;
+		this.room_no = room_no;
+	}
 
-	    public void setGuest_pno(String guest_pno) {
-	        this.guest_pno = guest_pno;
-	    }
+	public String getGuest_pno() {
+		return guest_pno;
+	}
 
-	    public int getM_count() {
-	        return m_count;
-	    }
+	public void setGuest_pno(String guest_pno) {
+		this.guest_pno = guest_pno;
+	}
 
-	    public void setM_count(int m_count) {
-	        this.m_count = m_count;
-	    }
+	public int getM_count() {
+		return m_count;
+	}
 
-	    public String getGuest_name() {
-	        return guest_name;
-	    }
+	public void setM_count(int m_count) {
+		this.m_count = m_count;
+	}
 
-	    public void setGuest_name(String guest_name) {
-	        this.guest_name = guest_name;
-	    }
+	public String getGuest_name() {
+		return guest_name;
+	}
 
-	    public int getStay_days() {
-	        return stay_days;
-	    }
+	public void setGuest_name(String guest_name) {
+		this.guest_name = guest_name;
+	}
 
-	    public void setStay_days(int stay_days) {
-	        this.stay_days = stay_days;
-	    }
+	public int getStay_days() {
+		return stay_days;
+	}
 
-	    public String getParking_yn() {
-	        return parking_yn;
-	    }
+	public void setStay_days(int stay_days) {
+		this.stay_days = stay_days;
+	}
 
-	    public void setParking_yn(String parking_yn) {
-	        this.parking_yn = parking_yn;
-	    }
+	public String getCar_no() {
+		return car_no;
+	}
 
-	    public int getRoom_no() {
-	        return room_no;
-	    }
+	public void setCar_no(String car_no) {
+		this.car_no = car_no;
+	}
 
-	    public void setRoom_no(int room_no) {
-	        this.room_no = room_no;
-	    }
+	public int getRoom_no() {
+		return room_no;
+	}
 
-	    @Override
-	    public String toString() {
-	        return "GuestDTO [guest_pno=" + guest_pno + ", m_count=" + m_count + ", guest_name=" + guest_name
-	                + ", stay_days=" + stay_days + ", parking_yn=" + parking_yn + ", room_no=" + room_no + "]";
-	    }
-	
+	public void setRoom_no(int room_no) {
+		this.room_no = room_no;
+	}
+
+	@Override
+	public String toString() {
+		return "GuestDTO [guest_pno=" + guest_pno + ", m_count=" + m_count + ", guest_name=" + guest_name
+				+ ", stay_days=" + stay_days + ", car_no=" + car_no + ", room_no=" + room_no + "]";
+	}
 
 }
