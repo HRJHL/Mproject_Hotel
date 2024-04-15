9b7468a97671e4f00c1b7e841d762864e12342c4
장호영 <53540017+jang643@users.noreply.github.com>
2024-04-15 14:18:10 +0900

    .

---
+++ sql/insertRoomData.sql
@@ -0,0 +1,16 @@
+/* 방 상태: 1 - 사용가능 2- 사용중 3 - 예약 4 - 수리*/
+insert into tb_room values(202, 1, 2, null, null);
+insert into tb_room values(203, 2, 2, '2024-04-15 11:11:11', '2024-04-16 12:00:00');
+insert into tb_room values(204, 1, 4, null, null);
+insert into tb_room values(205, 1, 5, null, null);
+insert into tb_room values(301, 3, 1, '2024-04-15 11:11:11', '2024-04-16 12:00:00');
+insert into tb_room values(302, 4, 3, '2024-04-15 11:11:11', '2024-04-16 12:00:00');
+insert into tb_room values(303, 1, 4, null, null);
+insert into tb_room values(304, 2, 2, '2024-04-15 11:11:11', '2024-04-16 12:00:00');
+insert into tb_room values(305, 2, 6, '2024-04-15 11:11:11', '2024-04-16 12:00:00');
+insert into tb_room values(401, 3, 1, '2024-04-15 11:11:11', '2024-04-16 12:00:00');
+insert into tb_room values(402, 4, 1, '2024-04-15 11:11:11', '2024-04-16 12:00:00');
+insert into tb_room values(403, 1, 1, null, null);
+insert into tb_room values(404, 1, 3, null, null);
+insert into tb_room values(405, 2, 4, '2024-04-15 11:11:11', '2024-04-16 12:00:00');
+insert into tb_room values(501, 1, 6, null, null);
