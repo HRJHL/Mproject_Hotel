8ea4452e18b66fc8644f9fa070a09bb4d219af52
장호영 <jang643@naver.com>
2024-04-16 00:02:15 +0900

    .

--- sql/Create_tables.sql
+++ sql/Create_tables.sql
@@ -1,7 +1,7 @@
 create table tb_room
 	( ROOM_NO int primary key, ROOM_STATE int CHECK(ROOM_STATE IN ( 1, 2, 3, 4 )), ROOM_CAPACITY int, ROOM_CHECKIN_TIME datetime, ROOM_CHECKOUT_TIME datetime);
 create table tb_guest
-	(GUEST_PNO varchar(20) primary key, M_COUNT int not null, GUEST_NAME varchar(20) not null, STAY_DAYS int not null, PARKING_YN varchar(1) not null, ROOM_NO int, constraint foreign key(ROOM_NO) references tb_room(ROOM_NO));
+	(GUEST_PNO varchar(20) primary key, M_COUNT int not null, GUEST_NAME varchar(20) not null, STAY_DAYS int not null, CAR_NO varchar(10), ROOM_NO int, constraint foreign key(ROOM_NO) references tb_room(ROOM_NO));
 create table tb_room_service
 	(BREAKFAST_YN varchar(1) not null, CLEANING_YN varchar(1) not null, DELIVERY_YN varchar(1) not null, ROOM_NO int, constraint foreign key(ROOM_NO) references tb_room(ROOM_NO));
     
---
+++ sql/insertGuestData.sql
@@ -0,0 +1,13 @@
+insert into tb_guest values('010-1111-2222', 1, '김철수', 3, '6548', 301);
+insert into tb_guest values('010-1212-3155', 1, '한아름', 3, null, 401);
+insert into tb_guest values('010-4581-6548', 3, '이보미', 3, null, 301);
+insert into tb_guest values('010-4567-1231', 1, '장민수', 3, '4567', 502);
+insert into tb_guest values('010-3544-4547', 4, '손승헌', 3, null, 503);
+insert into tb_guest values('010-1231-5468', 3, '최민수', 3, '5482', 504);
+insert into tb_guest values('010-0158-0865', 2, '성민헌', 3, '8715', 505);
+insert into tb_guest values('010-8468-8051', 6, '박강식', 3, '4567', 601);
+insert into tb_guest values('010-4658-4564', 5, '임시온', 3, null, 602);
+insert into tb_guest values('010-6487-4658', 2, '전경민', 3, '1234', 603);
+insert into tb_guest values('010-1324-7899', 3, '고지수', 3, null, 604);
+
+select * from tb_guest;
