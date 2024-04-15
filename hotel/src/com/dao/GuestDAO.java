c98672fe60021b03c66cc6ddf929ff32bcc8ac0c
장호영 <jang643@naver.com>
2024-04-15 10:24:03 +0900

    .

--- hotel/src/com/dao/GuestDAO.java
+++ hotel/src/com/dao/GuestDAO.java
@@ -1,6 +1,6 @@
 package com.dao;
 
-
+import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 
@@ -8,13 +8,29 @@ import com.dto.GuestDTO;
 
 public class GuestDAO {
 
-	
-	//save
+	// save
 	public int save(SqlSession session, GuestDTO dto) {
 		return session.insert("com.config.GuestMapper.save", dto);
 	}
-	//remove
-	//public int removeByEmpno(SqlSession session, int empno) {
-	//	return session.delete("com.config.EmpMapper.removeByEmpno", empno);
-	//}
+
+	// select
+	public List<GuestDTO> findAllGuest(SqlSession session) {
+		List<GuestDTO> list = session.selectList("com.config.HotelMapper.findAllGuest");
+		return list;
+	}
+
+	public List<GuestDTO> findGuestByPno(SqlSession session) {
+		List<GuestDTO> list = session.selectList("com.config.HotelMapper.findAllGuest");
+		return list;
+	}
+
+	// save
+	public int saveGuest(SqlSession session, GuestDTO dto) {
+		return session.insert("com.config.HotelMapper.saveGuest", dto);
+	}
+
+	// remove
+	public int removeByGuestPno(SqlSession session, int roomNo) {
+		return session.delete("com.config.HotelMapper.removeByGuestPno", roomNo);
+	}
 }
